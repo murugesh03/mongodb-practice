3a3dc2614c192195119c13faf3a839571397cebc
vishnumurugesh03 <105793802+vishnumurugesh03@users.noreply.github.com>
2022-07-25 09:34:19 +0530

    query for the mongoose is added

--- solution1.js
+++ solution1.js
@@ -4,12 +4,32 @@ mongoose.connect('mongodb://localhost/mongo-exercises');
 
 const courseSchema = new mongoose.Schema({
   _id: String,
-  name: String,
+  name: { type: String, required: true, minlength: 5, maxlength: 255 },
+  category: {
+    type: String,
+    required: true,
+    enum: ['web', 'network', 'mobile']
+  },
   author: String,
-  tags: [String],
+  tags: {
+    type: Array,
+    validate: {
+      validator: function (v) {
+        return v && v.length > 0;
+      },
+      message: 'A Course should have at least one tag.'
+    }
+  },
   date: Date,
   isPublished: Boolean,
-  price: Number
+  price: {
+    type: Number,
+    required: function () {
+      return this.isPublished;
+    },
+    min: 10,
+    max: 200
+  }
 });
 
 const Course = mongoose.model('Course', courseSchema);
@@ -86,10 +106,29 @@ async function deleteOne(id) {
   const result = await Course.deleteOne({ _id: id });
   console.log(result);
 }
-deleteOne('5a68fde3f09ad7646ddec17e');
+// deleteOne('5a68fde3f09ad7646ddec17e');
 
 async function deleteMany(id) {
   // const result = await Course.deleteMany({ _id: id });
   const result = await Course.findByIdAndDelete({ _id: id });
   console.log(result);
 }
+// deleteMany()
+async function createCourse() {
+  const course = new Course({
+    name: 'React Course',
+    category: 'web',
+    author: 'kishan kumar',
+    // tags: ['backend', 'frontend'],
+    tags: null,
+    isPublished: true,
+    price: 15
+  });
+  try {
+    const result = await course.save();
+    console.log(result, 'result');
+  } catch (error) {
+    console.log(error);
+  }
+}
+createCourse();
